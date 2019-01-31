1e67e407d2d03fcc3db2deaa9b26d70b3d03cd5e
Phuong Duong <phuongx31@gmail.com>
2019-01-31 23:04:42 +0700

    JS basic: linking js file, values and variables, data types

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,51 @@
+let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is FUN!')
+console.log(10 + 20 + 30 + 40);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Andrew";
+let first = "JJ";
+let firstNamePerson;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let first_name = "JD";
+let $function = 27;
+
+let person = "Andrew";
+let PI = 3.1415;
+
+let myFirstJob = "Programer";
+let mySecondJob = "Writer";
+
+let job1 = 'Programer';
+let job2 = 'Writer';
+
+console.log(myFirstJob);
+
+
+// data type
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 10);
+console.log(typeof 'Andrew');
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1995;
+console.log(typeof year);
+
+console.log(typeof null);
