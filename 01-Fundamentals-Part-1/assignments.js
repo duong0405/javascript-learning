ed6b3e8a5073735109e4fe3cb1ab86ec10107915
Phuong Duong <phuongx31@gmail.com>
2019-02-02 13:42:29 +0700

    assignment: lecture 1, 2, 3 and 4'

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,40 @@
+// LECTURE #1: Values and Variables
+const country = 'Vietnam';
+const continent = 'Asian';
+let population = 70000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// LECTURE #2: Data types
+const isIsland = false;
+let language;
+
+console.log(isIsland);
+console.log(population);
+console.log(population);
+console.log(language);
+
+// LECTURE #3: let, const, var
+language = 'vietnamese';
+population = 900000000;
+console.log(language);
+console.log(population);
+
+// LECTURE #4: Basic Operators
+const halfOfPopulation = population / 2;
+console.log(halfOfPopulation);
+
+console.log(population + 1);
+
+const FinlandPopulation = 6000000;
+const isMoreThanFinland = population > FinlandPopulation;
+console.log(isMoreThanFinland);
+
+const averagePopulation = 33000000;
+const isMoreThanAverage = population > averagePopulation;
+console.log(isMoreThanAverage);
+
+const description = 'Vietnam is in Asian, and its 90 million people speak Vietnamese';
+console.log(description);
