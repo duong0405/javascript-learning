c3bf2ad9fb6ed40122ead181f32dffe48c61ed12
Phuong Duong <phuongx31@gmail.com>
2019-02-02 12:43:34 +0700

    JS basic: let, const, var, operator

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -49,3 +49,56 @@ year = 1995;
 console.log(typeof year);
 
 console.log(typeof null);
+
+// let, const, var
+
+// let to define a variable that values can change later
+let age = 30;
+age = 31;
+
+// const to define a variable that not supported to change 
+const birthYear = 1995;
+// birthYear = 2000;
+// const job;
+
+
+// let vs var: block scode vs function scope ???
+// never use var
+var job = 'programer';
+job = 'writer';
+console.log(job);
+
+// Basic Operator
+
+// Math operator
+const now = 2030;
+const myAge = now - 1995;
+const ageAdrew = now - 1991;
+console.log(myAge, ageAdrew);
+
+console.log(myAge * 2, myAge / 2, 2 ** 3);
+
+const fName = "Dan";
+const lName = "Tae";
+console.log(fName + " " + lName);
+
+// Asignment operator
+let x = 10 + 5; // 15
+x += 10;        // x = x + 10 = 25
+x *= 4;         // x = x * 4 = 100
+x++;            // x = x + 1 = 101
+x--;
+x--;         
+console.log(x);
+
+// Comparison operator
+console.log(5 > 4);
+console.log(myAge > ageAdrew);
+console.log(myAge >= 18);
+
+let a, b;
+a = b = 25 - 10 - 5;
+console.log(a, b);
+
+const averageAge = (myAge + ageAdrew) / 2;
+console.log(averageAge);
