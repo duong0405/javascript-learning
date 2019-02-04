808c62ac22e3ad15f763d50845a5d8236267685c
Phuong Duong <phuongx31@gmail.com>
2019-02-04 18:00:05 +0700

    assignment lecture: 7,8,9,10,11

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -50,3 +50,54 @@ if (population > 33) {
 } else {
     console.log(`Vietnam's population is ${population} million below average`);
 }
+
+// LECTURE #7: type conversion and coercion
+const a = '9' - '5'; // expect 4
+const b = '19' - '13' + '17'; // expect '417'
+const c = '19' - '13' + 17;  // expect 21
+const d = '123' < 57;       // expect false
+const e = 5 + 6 + '4' + 9 - 4 - 2 // expect 1143
+console.log(a, b, c, d, e);
+
+// LECTURE #8: Equality operator
+// const numNeighbours = Number(prompt('How many neighbour coutries does your country have?'));
+// if (numNeighbours === 1) {
+//     console.log('Only 1 border!')
+// } else if (numNeighbours > 1) {
+//     console.log('More than 1 border');
+// } else {
+//     console.log('No borders');
+// }
+
+// LECTURE #9: Logical Operator
+if (language === 'vietnamese' && population > 50 && isIsland === false) {
+    console.log('You should live in Vietnam')
+} else {
+    console.log('Vietnam does not meet your criteria');
+}
+
+// LECTURE #10: switch statement
+switch(language) {
+    case 'chinese':
+    case 'mandiran':
+        console.log('MOST number of native speaker');
+        break;
+    case 'spanish':
+        console.log('2nd place in numbers of native speaker');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Greate language too');
+}
+
+// LECTURE #11: Tenary operator
+population > 33 ? console.log(`Vietnam's population is above average`) : console.log(`Vietnam's population is below average`);
+
